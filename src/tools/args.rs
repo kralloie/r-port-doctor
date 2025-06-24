43b9f2ae1c69d0c2dc0710d6e725f26b04e553c3
Franco Gargiulo <francogargiulo2004@gmail.com>
2025-06-24 05:16:50 -0300

    Add help description to args

--- src/tools/args.rs
+++ src/tools/args.rs
@@ -3,19 +3,19 @@ use clap::{arg, Parser};
 #[derive(Parser, Debug)]
 #[command(name = "r-port-doctor", version, about = "Port debug and diagnostic tool", ignore_errors = true)]
 pub struct Args {
-    #[arg(short = 'p', long)]
+    #[arg(short = 'p', long, help = "Filter by local port")]
     pub port: Option<u16>,
 
-    #[arg(short = 'm', long)]
+    #[arg(short = 'm', long, help = "Filter by protocol (UDP/TCP)")]
     pub mode: Option<String>,
 
-    #[arg(short = 'n', long = "process-name")]
+    #[arg(short = 'n', long = "process-name", help = "Filter by similar process name")]
     pub process_name: Option<String>,
 
-    #[arg(short = 'i', long)]
+    #[arg(short = 'i', long, help = "Filter by PID")]
     pub pid: Option<u32>,
 
-    #[arg(short = 's', long)]
+    #[arg(short = 's', long, help = "Filter by socket state (LISTEN, ESTABLISHED, etc)")]
     pub state: Option<String>
 }
 
