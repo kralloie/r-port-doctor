aa04c01126e531a78ad8b9c2068ad01317233898
Franco Gargiulo <francogargiulo2004@gmail.com>
2025-06-30 14:39:13 -0300

    Clean up main file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use clap::Parser;
 use r_port_doctor::tools::args::Args;
-use r_port_doctor::tools::socket::{self};
+use r_port_doctor::tools::socket::{Socket};
 use r_port_doctor::tools::tcp_table::{get_tcp_sockets, get_tcp_sockets_ipv6};
 use r_port_doctor::tools::udp_table::{get_udp_sockets, get_udp_sockets_ipv6};
 fn main() {     
     let args = match Args::try_parse() {
         Ok(args) => args,
         Err(e) => {
-            eprintln!("Invalid arguments: {}", e);
+            eprintln!("{}", e);
             std::process::exit(0);
         }
     };
     let argc = args.get_argc();
-    let mut sockets: Vec<socket::Socket>;
-    match &args.ip_version {
+    let mut sockets: Vec<Socket>;
+    match args.ip_version {
         Some(version) => {
-            if *version == 6 {
+            if version == 6 {
                 sockets = get_tcp_sockets_ipv6();
                 sockets.extend(get_udp_sockets_ipv6());
             } else {
@@ -28,6 +28,6 @@ fn main() {
             sockets.extend(get_udp_sockets());
         }
     }
-    socket::Socket::sort_socket_table(&mut sockets, &args);
-    socket::Socket::print_socket_table(&sockets, &args, argc);
+    Socket::sort_socket_table(&mut sockets, &args);
+    Socket::print_socket_table(&sockets, &args, argc);
 }
