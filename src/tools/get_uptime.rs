511b109322e17183b0e44d2c1ee0aac04d07563d
Franco Gargiulo <francogargiulo2004@gmail.com>
2025-07-08 14:06:27 -0300

    Make CURRENT_FILETIME static with lazylock

--- src/tools/get_uptime.rs
+++ src/tools/get_uptime.rs
@@ -1,11 +1,11 @@
 use windows::Win32::System::SystemInformation::GetSystemTimeAsFileTime;
+use std::sync::LazyLock;
 
-fn get_current_filetime() -> u64 {
+static CURRENT_FILETIME: LazyLock<u64> = LazyLock::new(|| {
     let ft = unsafe { GetSystemTimeAsFileTime() };
     ((ft.dwHighDateTime as u64) << 32) | (ft.dwLowDateTime as u64)
-}
+});
 
 pub fn get_socket_uptime(socket_timestamp: i64) -> u64 {
-    let current_filetime = get_current_filetime();
-    (current_filetime - socket_timestamp as u64) / 10_000_000 // Nano-seconds to seconds
+    (*CURRENT_FILETIME - socket_timestamp as u64) / 10_000_000 // Nano-seconds to seconds
 }
