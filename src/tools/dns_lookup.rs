3c5e19c942ef082842d739987080f5eba3cf7f32
Franco Gargiulo <francogargiulo2004@gmail.com>
2025-07-10 17:47:17 -0300

    Use DashMap instead HashMap for resolved hostnames and implement multi-threading for faster hostname resolving

--- src/tools/dns_lookup.rs
+++ src/tools/dns_lookup.rs
@@ -1,15 +1,12 @@
-use std::{collections::HashMap, sync::{LazyLock, Mutex}};
-
+use std::{sync::LazyLock, thread};
 use dns_lookup::lookup_addr;
-
 use crate::tools::socket::Socket;
+use dashmap::DashMap;
 
-static DNS_CACHE: LazyLock<Mutex<HashMap<String, String>>> = LazyLock::new(|| Mutex::new(HashMap::new()));
+static DNS_CACHE: LazyLock<DashMap<String, String>> = LazyLock::new(|| DashMap::new());
 
 pub fn lookup_address(ip: &str) -> String {
-    let mut cache = DNS_CACHE.lock().unwrap();
-
-    if let Some(cached) = cache.get(ip) {
+    if let Some(cached) = DNS_CACHE.get(ip) {
         return cached.clone()
     }
 
@@ -19,17 +16,42 @@ pub fn lookup_address(ip: &str) -> String {
         .and_then(|parsed_ip| lookup_addr(&parsed_ip).ok())
         .unwrap_or_else(|| ip.to_string());
 
-    cache.insert(ip.to_string(), hostname.clone());
+    DNS_CACHE.insert(ip.to_string(), hostname.clone());
     hostname
 }
 
 pub fn resolve_socket_table_addresses(sockets: &mut Vec<Socket>) {
-    sockets.iter_mut().for_each(|s|  {
-        if s.protocol == "UDP" {
-            return
-        }
-        if let Some(addr) = &s.remote_addr {
-            s.remote_addr = Some(lookup_address(&addr));
-        }
-    });
+    let threads = 4;
+    let len = sockets.len();
+    let chunk_size = (len + threads - 1) / threads;
+
+    let mut handles = Vec::new();
+
+    let chunks: Vec<Vec<Socket>> = sockets
+        .drain(..)
+        .collect::<Vec<_>>()
+        .chunks(chunk_size)
+        .map(|chunk| chunk.to_vec())
+        .collect();
+
+    for mut chunk in chunks {
+        let handle = thread::spawn(move || {
+            for s in &mut chunk {
+                if s.protocol == "UDP" {
+                    continue;
+                }
+                if let Some(addr) = &s.remote_addr {
+                    s.remote_addr = Some(lookup_address(addr));
+                }
+            }
+            chunk
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        let chunk = handle.join().unwrap();
+        sockets.extend(chunk);
+    }
 }
